6b45c81c5aa074ac2d1426a2fb12731a45f80e6b
adamggrover <93373074+adamggrover@users.noreply.github.com>
2022-09-04 00:27:02 +0100

    create basic starting template for web projects

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const { src, dest, watch, series } = require('gulp');
+const postcss = require('gulp-postcss');
+const sharpResponsive = require("gulp-sharp-responsive");
+
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const autoprefixer = require('autoprefixer');
+const browsersync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+
+// Sass Task
+function scssTask(){
+    return src('src/scss/styles.scss', {sourcemaps : true})
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('dist/css', {sourcemaps: '.'}));
+}
+
+// Js Task
+function jsTask(){
+    return src('src/js/main.js', {sourcemaps : true})
+    .pipe(terser())
+    .pipe(dest('dist/js', {sourcemaps: '.'}));
+
+}
+
+
+
+// Brosersync Tasks
+function browsersyncServe(cb){
+    browsersync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    cb();
+
+}
+
+function browsersyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+
+//Sharp Responsive Task
+
+const img = () => src("src/assets/images/**/*.{jpg,png}")
+  .pipe(sharpResponsive({
+    formats: [
+      { width: 440, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-sm" } },
+      { width: 740, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-md" } },
+      { width: 1024, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-lg" } },
+      { width: 1800, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-xl" } },
+    ]
+  }))
+  .pipe(dest("dist/images"));
+
+// Watch Task
+function watchTask(){
+    watch('*html', browsersyncReload);
+    watch(['src/scss/**/*.scss', 'src/js/**/*.js', "src/assets/images/**/*.{jpg,png}"]), series(scssTask, jsTask, browsersyncReload);
+
+}
+
+// Default Gulp Task
+exports.default = series(
+    scssTask,
+    jsTask,
+    img,
+    browsersyncServe,    
+    watchTask
+)
